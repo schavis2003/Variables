c77fc38010787a6a2388876a88b7b126fc23f7f3
schavis2022 <schavis2022>
2022-06-07 17:31:08 -0400

    Variable

---
+++ Variable_Exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variable_Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "This is a string of words";
+            int number = 0;
+            char letter = 'A';
+            bool isCodeFun = true;
+            double testing = 0.0;
+            decimal alsoTesting = 0m;
+            Console.WriteLine($"{word} and here is a number {number}");
+        }
+    }
+}
